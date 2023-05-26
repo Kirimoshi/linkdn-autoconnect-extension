05638825446f3d2f57441bb6b3ff7bf4731f2447
Kirimoshi <baronaxaro@gmail.com>
2023-05-26 21:15:11 +0300

    Fixed content script injection

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,15 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Background script is running");
   console.log(message);
+  chrome.tabs
+    .query({
+      active: true,
+      currentWindow: true,
+    })
+    .then((tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["contentScript.js"],
+      });
+    });
 });
